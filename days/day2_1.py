9e540032c8d3a1c487158c3581b923fb41f06752
Carlos Martinez <ctmartinez1992@gmail.com>
2023-12-04 20:28:08 +0000

    Day 2 Part 1

---
+++ days/day2_1.py
@@ -0,0 +1,47 @@
+from base import *
+
+
+red_limit = 12
+green_limit = 13
+blue_limit = 14
+
+
+def process(line):
+    game_string = line.split(": ")
+    game_id = int(game_string[0].split("Game ")[1])
+
+    r, g, b = 0, 0, 0
+
+    configs = game_string[1].split("; ")
+    for config in configs:
+        cubes = config.split(", ")
+        for cube in cubes:
+            cube_value, cube_name = cube.split(" ")
+            if "red" in cube_name:
+                if int(cube_value) > r:
+                    r = int(cube_value)
+            if "green" in cube_name:
+                if int(cube_value) > g:
+                    g = int(cube_value)
+            if "blue" in cube_name:
+                if int(cube_value) > b:
+                    b = int(cube_value)
+
+    return game_id, r, g, b
+
+
+def final(values):
+    result = 0
+    for game_id, r, g, b in values:
+        if r <= red_limit and g <= green_limit and b <= blue_limit:
+            result += game_id
+    return result
+
+
+print(
+    do_func_for_each_line_in_file(
+        f"inputs/{Path(os.path.basename(__file__)).stem}.txt",
+        process,
+        final,
+    )
+)
