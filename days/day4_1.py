17370d7dfebf4b3cd8cbc1939bd1f5abf85f9e14
Carlos Martinez <ctmartinez1992@gmail.com>
2023-12-05 08:04:53 +0000

    Day 4 Part 1

---
+++ days/day4_1.py
@@ -0,0 +1,44 @@
+from base import *
+import re
+from collections import defaultdict
+
+
+def get_all_numbers(line):
+    numbers = []
+    splitted = line.split("  ")
+    for split in splitted:
+        final = split.split(" ")
+        numbers.extend([int(f) for f in final if f])
+    return numbers
+
+
+def process(i, line):
+    game_string, number_string = line.split(":")
+    game_id = int(game_string[5:8])
+    winning_string, number_string = number_string.split("|")
+
+    return game_id, get_all_numbers(winning_string), get_all_numbers(number_string)
+
+
+def final(results):
+    final = 0
+    for result in results:
+        card_result = 0
+        game_id, winning_numbers, card_numbers = result
+        for n in card_numbers:
+            if n in winning_numbers:
+                if card_result == 0:
+                    card_result = 1
+                else:
+                    card_result = card_result * 2
+        final += card_result
+    return final
+
+
+print(
+    do_func_for_each_line_in_file(
+        f"inputs/{Path(os.path.basename(__file__)).stem}.txt",
+        process,
+        final,
+    )
+)
