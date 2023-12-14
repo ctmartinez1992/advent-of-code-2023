dcb53766ba213413bf2cedd8121aea4c94b085bd
Carlos Martinez <ctmartinez1992@gmail.com>
2023-12-14 08:10:02 +0000

    Day 13 Part 2

---
+++ days/day13_2.py
@@ -0,0 +1,28 @@
+from base import *
+
+from collections import defaultdict
+
+
+def bset(lines):
+	s = set()
+	for r, line in enumerate(lines):
+		for c, char in enumerate(line):
+			s.add((r, c, char))
+	return s
+
+
+def mirror(pattern, index):
+	part1, part2 = zip(*zip(pattern[index:], reversed(pattern[:index])))
+	return len(bset(part1) - bset(part2))
+
+
+def solve():
+	patterns = [pattern.splitlines() for pattern in open("inputs/day13_1.txt").read().split("\n\n")]
+	summary = defaultdict(int)
+	for pat in patterns:
+		for mult, pattern in [(100, pat), (1, list(zip(*pat)))]:
+			for index in range(1, len(pattern)):
+				summary[mirror(pattern, index)] += mult * index
+	return summary[1]
+	
+print(solve())
